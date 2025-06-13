befeb9021a466f55c4258b7ac66291bfa24675e9
Pedro Dias Soares <pedro3soares@gmail.com>
2025-06-13 08:18:07 -0300

    correção

--- app/main.py
+++ app/main.py
@@ -5,8 +5,15 @@ from .endpoints import configure_routes
 def create_app():
     """Cria e configura uma instância da aplicação Flask."""
     app = Flask(__name__)
-    
+
     # Carrega o modelo e configura as rotas
     configure_routes(app)
-    
+
     return app
+
+# Bloco de código para depuração manual - adicione ao final de app/main.py
+if __name__ == '__main__':
+    # Chama a nossa função de fábrica para obter a aplicação
+    app = create_app()
+    # Roda a aplicação diretamente com o modo de depuração do Flask ativado
+    app.run(debug=True)
